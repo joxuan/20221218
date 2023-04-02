0be7fb48ebea9dbbb5eec24662ee79a0eb704c85
joxuan <t531121@gmail.com>
2023-04-02 16:47:03 +0800

    API 串接 todo

--- dist/js/todo_vue.js
+++ dist/js/todo_vue.js
@@ -6,6 +6,7 @@ createApp({
             storageKey: 'todo',
             itemValue: '',
             items: [],
+            api: 'https://book.niceinfos.com/frontend/api/'
         }
     },
     methods: {
@@ -67,6 +68,47 @@ createApp({
         save() {
             let data = JSON.stringify(this.items);
             localStorage.setItem(this.storageKey, data);
+        },
+        doSaveCloud() {
+            let uid = prompt('請輸入 uid');
+            if (!uid) {
+                return;
+            }
+
+            let params = {
+                action: 'todo',
+                uid: uid,
+                data: this.items
+            }
+
+            let options = {
+                method: 'POST',
+                body: JSON.stringify(params),
+            }
+
+            fetch(this.api, options)
+                .then(response => {
+                    return response.text();
+                })
+                .then(data => {
+                    console.log(data);
+                })
+        },
+        doLoadCloud() {
+            let uid = prompt('請輸入 uid');
+            if (!uid) {
+                return;
+            }
+
+            let api = `${this.api}?action=todo&uid=${uid}`;
+            fetch(api)
+                .then(response => {
+                    return response.text();
+                })
+                .then(data => {
+                    data = JSON.parse(data);
+                    this.items = data.data;
+                })
         }
     },
     mounted() {
